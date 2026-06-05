f964aa8f3fbe703e4e27bf44dbbde6cf0ca4e09a
Caroline Bvlt <caroline.bieuvelet@gmail.com>
2026-06-05 16:40:40 +0200

    working with files

---
+++ working_with_files.py
@@ -0,0 +1,7 @@
+with open("./generated_files/test.txt", "w") as file :
+    myText = "Hello world !"
+    file.write(myText)
+
+with open("./generated_files/test.txt", "r") as file :
+    text = file.read()
+    print(text)
