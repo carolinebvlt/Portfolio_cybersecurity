b6c28d84b1624eb5dc2fee244cf273ceef075da5
Caroline Bvlt <caroline.bieuvelet@gmail.com>
2026-06-13 17:35:18 +0200

    calculate duration between first and last failed connection for each IP alert

--- working_with_files/SSH_log_analyzer/main.py
+++ working_with_files/SSH_log_analyzer/main.py
@@ -10,6 +10,8 @@ path = os.path.join(base_dir, "ssh_logs.txt")
 with open(path, "r") as file:
     logs = file.readlines()
 
+# STEP 1 : PARSING ---------------------------------------------------------
+
 # Init a list of dictionnaries (one dict for each log)
 list_dict_logs = []
 
@@ -46,8 +48,9 @@ for log in logs :
         "protocol" : splited_log[-1]
     })
 
+# STEP 2 : COUNTING ----------------------------------------------------
 
-# Now, only working with le list of dictionnaries list_dict_logs
+# Now, only working with the list of dictionnaries list_dict_logs
 
 # Count failed and accepted password
 accepted_password = 0
@@ -57,6 +60,7 @@ ip_addresses_failed_password = []
 hosts_failed_password = []
 users_failed_password = []
 protocols_failed_password = []
+dict_ip_timestamps = {}
 
 for log_dict in list_dict_logs :
     event = log_dict.get("event")
@@ -67,7 +71,12 @@ for log_dict in list_dict_logs :
         hosts_failed_password.append(log_dict.get("host"))
         users_failed_password.append(log_dict.get("user"))
         protocols_failed_password.append(log_dict.get("protocol"))
-        
+
+        #create new dictionnary with ip as keys and list of timestamps as value
+        if log_dict.get("ip") not in dict_ip_timestamps :
+            dict_ip_timestamps[log_dict.get("ip")] = []
+        dict_ip_timestamps[log_dict.get("ip")].append(log_dict.get("timestamp"))
+
     elif event == "Accepted password":
         accepted_password += 1
 
@@ -76,6 +85,8 @@ count_ip_addresses = Counter(ip_addresses_failed_password)
 count_users = Counter(users_failed_password)
 count_hosts = Counter(hosts_failed_password)
 count_protocols = Counter(protocols_failed_password)
+
+# STEP 3 : DISPLAY ---------------------------------------------------
 
 # display most common IP for all failed attempts
 total_failed_attempts = 0
@@ -92,17 +103,37 @@ for user, count in count_users.most_common() :
 for host, count in count_hosts.most_common():
     print(f"The host {host} was targeted {count} times.")
 
-# display most common protocol far all failed attempts
+# display most common protocol for all failed attempts
 for protocol, count in count_protocols.most_common():
     print(f"The protocol {protocol} has been used for a failed login attempt {count} times.")
 
 # display the timestamp of the first and the last failed occurence
-print(f"The first failed occured at {failed_logs[0]["timestamp"]}.")
-print(f"The last failed occured at {failed_logs[-1]["timestamp"]}.")
+print(f"The first failed occured at {failed_logs[0]['timestamp']}.")
+print(f"The last failed occured at {failed_logs[-1]['timestamp']}.")
 
 # calculate the time between these two timestamp
-first_dt = datetime.strptime("2026 " + failed_logs[0]["timestamp"], "%Y %b %d %H:%M:%S")
-last_dt = datetime.strptime("2026 " + failed_logs[-1]["timestamp"], "%Y %b %d %H:%M:%S")
+first_dt = datetime.strptime("2026 " + failed_logs[0]['timestamp'], "%Y %b %d %H:%M:%S")
+last_dt = datetime.strptime("2026 " + failed_logs[-1]['timestamp'], "%Y %b %d %H:%M:%S")
 
 # display the difference between the two datetime objects
-print(f"{total_failed_attempts} failed attempts occured in (time) : {last_dt - first_dt}")
+duration = int(last_dt.timestamp()) - int(first_dt.timestamp())
+print(f"{total_failed_attempts} failed attempts occured in (time) : {duration} seconds, or {duration/60} minutes")
+
+# STEP 4 : ANALYZING / ALERT -------------------------------------------------
+print("--------------------------------")
+# ALERT if more than 10 failed attemps for ips and users
+for ip, count in count_ip_addresses.most_common() :
+    if count >= 10:
+        print(f"ALERT : suspicious activity. {count} failed attempts for the IP {ip}")
+        print(f"First attempt at : {dict_ip_timestamps.get(ip)[0]}")
+        print(f"Last attempt at : {dict_ip_timestamps.get(ip)[-1]}")
+        # convert to calculate the diff bewteen the two timestamps
+        first_dt = datetime.strptime("2026 " + dict_ip_timestamps.get(ip)[0], "%Y %b %d %H:%M:%S")
+        last_dt = datetime.strptime("2026 " + dict_ip_timestamps.get(ip)[-1], "%Y %b %d %H:%M:%S")
+        duration = int(last_dt.timestamp()) - int(first_dt.timestamp())
+        print(f"Duration between first and last attempt : {duration} seconds, or {duration/60} minutes")
+
+for user, count in count_users.most_common():
+    if count >= 10:
+        print(f"ALERT : suspicious activity. {count} failed attempts for the user {user}")
+
