8b1e9d6d7e9fa633e50fcc483db10fabd1682f39
Caroline Bvlt <caroline.bieuvelet@gmail.com>
2026-06-14 15:36:51 +0200

    fix creation directory in storage

--- File Integrity Monitor/storage.py
+++ File Integrity Monitor/storage.py
@@ -1,7 +1,11 @@
 import json
+import os
 
 
 def storage(files_hashes_dictionnary, dir_to_analyze) :
+
     json_name = f"baseline_{dir_to_analyze}"
-    with open(json_name, "w") as file :
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(base_dir, json_name)
+    with open(path, "w") as file :
         json.dump(files_hashes_dictionnary, file, indent=4)
