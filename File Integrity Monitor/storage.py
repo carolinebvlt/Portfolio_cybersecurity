2943d3e160c7156bbfdaf9273ec4bd3ee96716e2
Caroline Bvlt <caroline.bieuvelet@gmail.com>
2026-06-14 15:56:50 +0200

    add .json ext to created files

--- File Integrity Monitor/storage.py
+++ File Integrity Monitor/storage.py
@@ -4,7 +4,7 @@ import os
 
 def storage(files_hashes_dictionnary, dir_to_analyze) :
 
-    json_name = f"baseline_{dir_to_analyze}"
+    json_name = f"baseline_{dir_to_analyze}.json"
     base_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(base_dir, json_name)
     with open(path, "w") as file :
